0d2be720164a9704717224f93951a8baa0673aac
surendrap720 <surendrap720@gmail.com>
2018-09-21 23:44:42 +0530

    changes done to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 
 const express = require('express');
 const router = express.Router();
+const tutor = require('../models/tutors');
 
 router.get('/', (req, res) => {
-	res.render('index');
+	tutor.find( { }, function(err, docs){	
+      var tutorChunks = [];	
+      var chunkSize = 3;	
+      for(var i=0; i < docs.length; i+= chunkSize){	
+          tutorChunks.push(docs.slice(i, i+chunkSize));	
+      }	
+        res.render('index', {  tutors: tutorChunks });	
+      	
+    });	
+	//res.render('index');
 });
 
 

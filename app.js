5f2eb99141a23d5708a1acef6a9300b8c59b08b8
Puneeth Potu <puneeth@nerdwallet.com>
2018-09-21 10:16:49 -0700

    Pushing prachi changes

--- app.js
+++ app.js
@@ -4,32 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressHandlebars = require('express-handlebars');
-const mongoose = require('mongoose');
-const passport  = require('passport');
-const http = require('http');
-const requestIp = require('request-ip');
-const where = require('node-where');
-var iplocation = require('iplocation');
-
-
-mongoose.Promise = global.Promise;
-
-
-//database connection 
-
-
-
-
-mongoose.connect('mongodb://root:root123@ds251362.mlab.com:51362/tutorry', { useNewUrlParser: true}, function(err){
-
-  if(err){
-    console.log(err);
-  } else {
-    console.log('Connected to the database');
-  }
-});
-
-
 const app = express();
 app.use(morgan('dev'));
 
@@ -42,55 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*const ipMiddleware = function(req, res, next) {
-    const clientIp = requestIp.getClientIp(req); 
-    next();
-};*/
-
-/*app.use(requestIp.mw())
-app.use(function(req, res) {
-    const ip = req.clientIp;
-    console.log(ip);
-
-    res.end(ip);
-});
-
- where.is('10.14.28.230', function(err, result) {
-  if (result) {
-  
-  console.log(result);
-    console.log('Country: ' + result.get('country'));
-  
-  }
-});*/
-
-/*iplocation('198.168.1.207', function (error, res) {
- 
- if(error){
-  console.log(error);
-}
-  else{
-    console.log(res);
-  }
- 
-  });*/
-
-
-
-
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-
-  res.render('notFound');
-});
-
-
 app.listen(4000, () => console.log('Server started listening on port 4000!'));
-
-
-
-
---
+++ routes/js/new.js
@@ -0,0 +1,28 @@
+
+
+
+<script>
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button1");
+var tabPanels=document.querySelectorAll(".tabContainer .tabPanel");
+
+function.showPanel(panelIndex,colorCode){
+tabButtons.forEach(function(node){
+	node.style.backgroundColor="";
+	node.style.color="";
+});
+tabButtons[panelIndex].style.backgroundColor=colorCode;
+tabButtons[panelIndex].style.color="white";
+
+tabPanels.forEach (function(node)
+{
+	node.style.display="none";
+});
+
+tabPanels[panel Index].style.display="block";
+tabPanels[panel Index].style.backgroundColor=colorCode;
+
+}
+showPanel(0,'#f44336');
+
+
+</script>

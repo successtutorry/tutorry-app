c3c1935751029826948753fb2e64ac69210de44c
surendrap720 <surendrap720@gmail.com>
2018-09-28 23:22:13 +0530

    added routes for contact form everything is fine

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,52 @@ const express = require('express');
 const router = express.Router();
 
 
+
+
+router.route('/contacts')
+  .get((req, res) => {
+    res.render('contacts');
+  }).post((req, res) => {
+    
+      const result = Joi.validate(req.body, contactusSchema);
+      const contact_message = result.value.message;
+      if (result.error) {
+          console.log(result.error);
+        
+        res.redirect('/users/contacts');
+      
+      }
+
+      const newContact = new ContactusForm(result.value); 
+      console.log('newContact', newContact);
+      newContact.save();
+
+      var rand = Math.floor((Math.random() * 100) + 54);
+      const link = "http://127.0.0.1:4000/users/verify?id="+result.value.email;
+
+      const html = `Hi there,
+      <br/>
+      Thank you for contacting us 
+      </br></br>     
+      We have recieved your message.
+      </br>
+      <b>${contact_message}</b>
+      <br/> </br>
+
+      <a href="${link}">${contact_message}</a> 
+      </br></br>   
+      We will reach back to you soon.
+      <br/><br/>
+      Have a pleasant day.` 
+
+      // Send email
+      mailer.sendEmail('tutorry.in@gmail.com', result.value.email, 'Message', html);
+      res.redirect('/users/contacts');
+
+
+  });
+
+
 router.route('/inner')
   .get((req, res) => {
     res.render('inner');
@@ -20,10 +66,7 @@ router.route('/become_tutor')
   .get((req, res) => {
     res.render('become_tutor');
   })
-  router.route('/contacts')
-  .get((req, res) => {
-    res.render('contacts');
-  })
+  
 
   router.route('/register')
   .get((req, res) => {

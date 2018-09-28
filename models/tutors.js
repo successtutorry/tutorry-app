dc525d6177615fcdea5c5c8b9c551751eef326a9
surendrap720 <surendrap720@gmail.com>
2018-09-29 00:11:16 +0530

    added location in the schema

--- models/tutors.js
+++ models/tutors.js
@@ -6,7 +6,8 @@ const tutorSchema = new Schema({
 	price: String,
 	email: String,
 	name: String,
-	subjects:String
+	subjects:String,
+	location:String,
 }, {
 
 	timestamps: { // this will give us the detail when the form was send.
